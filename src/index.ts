6066dfe182b600ce2a48e74ad2ad94a9cc78f6b5
faroukm2002 <faroukm238@gmail.com>
2025-01-07 14:54:02 +0200

    update index and auth

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-import express  from "express";
+import express, { NextFunction }  from "express";
 import dotenv from "dotenv";
 import { dbConnection } from "../database/dbConnection";
 import { Request, Response } from "express";
 import userRouter from "./modules/user/user.routes";
 import authRouter from "./modules/auth/auth.routes";
+import { AppError } from "./utils/AppError";
+import { globalError } from "./middleware/globalErrorMiddleware";
 
 dotenv.config()
 
 const app = express();
-  app.use("/api/v1/auth", authRouter);
-  app.use("/api/v1/user", userRouter);
-
+ 
 // Middleware for JSON body parsing
-app.use(express.json({limit: "10kb"}));
-app.use(express.urlencoded({extended: false}));
+app.use(express.json({ limit: "10kb" })); // Parses JSON request bodies
+app.use(express.urlencoded({ extended: false })); // Parses URL-encoded request bodies
 
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", userRouter);
 
 
 
@@ -31,3 +33,10 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, ():void =>
     console.log(`Example app listening on port http://localhost:${port}`)) 
  
+  // Handle invalid URLs
+  app.use("*", (req:Request, res:Response, next:NextFunction) => {
+    next(new AppError(`Invalid URL ${req.originalUrl}`, 404));
+  });
+
+  // Global Error Handling Middleware
+  app.use(globalError);
